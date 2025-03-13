7483da444319a1d91feb616d523aabb4aae91d69
eimiz <e@git.com>
2025-03-13 12:09:02 +0200

    Added station.h

---
+++ station.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+typedef enum {TEMPR_EVENT = 0, MOTION_EVENT, CHECKCHARGE_EVENT, BLINK_EVENT, BLINK2_EVENT, BLINK3_EVENT, SIMPROCESS_EVENT, SIMRX_WATCH_EVENT, TEMPR_STREAM_EVENT, TEMPR_MEASURE_EVENT} TEvent;
+
+typedef void (*TaskFunc)(void *);
+typedef struct {
+   const TEvent event;
+   const TaskFunc func;
+   //when period = 0 - EVENT must be set manually to execute the task
+    uint32_t period;
+    uint32_t lastTick;
+    bool active;
+} Task;
+
+void stationStartTask(Task *task);
+void stationStopTask(Task *task);
